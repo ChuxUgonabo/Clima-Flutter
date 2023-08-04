74ce309e129109e45b2db7dea3e2101fe82016c7
John_Ugonabo <chuxugonabo@gmail.com>
2023-08-04 15:11:18 -0700

    - Created properties to hold the latitude and longitude. - Created the getLocationData() to interpolate the apiURL and call the NetworkHelp to retrieve the API response

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -9,6 +9,27 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  double? latitute;
+  double? longitude;
+
+  @override
+  void initState() {
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    longitude = location.longitude;
+    latitute = location.latitude;
+
+    final String apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=$latitute&lon=$longitude&appid=$apiKey';
+    NetworkHelper networkHelper = NetworkHelper(apiUrl);
+
+    var weatherData = await networkHelper.getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
