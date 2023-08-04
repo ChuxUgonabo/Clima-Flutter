536dec45bcee196e394a907abe3489f118bbeaa6
John_Ugonabo <chuxugonabo@gmail.com>
2023-08-04 15:08:03 -0700

    import packages and create apiKey constant

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
 
+const apiKey = '4b6d0397609297c2ad97c93bdbddc541';
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
