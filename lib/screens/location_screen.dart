8dfd5bb5f4dffba7951840e24e51286ddf74598c
John_Ugonabo <chuxugonabo@gmail.com>
2023-08-08 12:47:43 -0700

    Added validation to check when weatherData is null and handle with error message

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -20,6 +20,13 @@ class _LocationScreenState extends State<LocationScreen> {
 
   void updateUI(dynamic weatherData) {
     setState(() {
+      if (weatherData == null) {
+        temperature = 0;
+        weatherMessage = 'Error with the weatherData';
+        weatherIcon = 'Error';
+        cityName = '';
+        return;
+      }
       double temp = weatherData['main']['temp'];
       temperature = temp.toInt();
       var condition = weatherData['weather'][0]['id'];
@@ -57,7 +64,10 @@ class _LocationScreenState extends State<LocationScreen> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   TextButton(
-                    onPressed: () {},
+                    onPressed: () async {
+                      var weatherData = await weatherModel.getLocationWeather();
+                      updateUI(weatherData);
+                    },
                     child: Icon(
                       Icons.near_me,
                       size: 50.0,
