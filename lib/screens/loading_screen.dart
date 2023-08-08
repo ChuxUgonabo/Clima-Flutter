c22201774bbbd7a9176119741d7cad62fcb23769
John_Ugonabo <chuxugonabo@gmail.com>
2023-08-08 11:43:10 -0700

    Refactored LoadingScreen and added Navigation to the LocationScreen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -12,9 +12,6 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
-  double? latitute;
-  double? longitude;
-
   @override
   void initState() {
     getLocationData();
@@ -23,13 +20,18 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
-    longitude = location.longitude;
-    latitute = location.latitude;
 
-    final String apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=$latitute&lon=$longitude&appid=$apiKey';
+    final String apiUrl =
+        'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric';
     NetworkHelper networkHelper = NetworkHelper(apiUrl);
 
     var weatherData = await networkHelper.getData();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
+    }));
   }
 
   @override
