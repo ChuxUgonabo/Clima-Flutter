1ccfbcb3c22d7911ad73683c360f497e7b2e3671
John_Ugonabo <chuxugonabo@gmail.com>
2023-08-08 11:40:07 -0700

    Added flutter_spinkit to display a loading UI when API is being retrived

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const apiKey = '4b6d0397609297c2ad97c93bdbddc541';
 class LoadingScreen extends StatefulWidget {
@@ -34,11 +36,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
         ),
       ),
     );
